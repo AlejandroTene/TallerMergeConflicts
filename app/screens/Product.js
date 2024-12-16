5e39e8a4861cdd2b93691f562f6bb626a2aa19e7
ALEK21DS <brandonalexpesantez@gmail.com>
2024-12-16 09:14:14 -0500

    Modificar Products.js

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,12 +2,16 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text style={styles.title}>Bienvenidos a mi tienda, soy Brandon Pesantez</Text>
+        <Text style={styles.text}>1. Doritos</Text>
+        <Text style={styles.text1}>2. Cheetos</Text>
+        <Text style={styles.text2}>3. Rufles</Text>
         <Button
             title='HOME'
             onPress={()=>{
                 navigation.navigate('HomeNav');
             }}
+            color="black"
         />
     </View>
 
@@ -16,8 +20,23 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: 'orange',
       alignItems: 'center',
       justifyContent: 'center',
+    },title: {
+        color: 'black',
+        fontSize: 20,
     },
+    text: {
+        color: 'blue',
+        fontSize: 15,
+    },
+    text1: {
+        color: 'red',
+        fontSize: 15,
+    },
+    text2: {
+        color: 'green',
+        fontSize: 15,
+    }
   });
